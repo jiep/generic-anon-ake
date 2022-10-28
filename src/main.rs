0240f6ecfbe8eced783b4997fbe4e83668622c21
José Ignacio Escribano <jiep@users.noreply.github.com>
2022-10-28 15:27:04 +0000

    Add gcm

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,32 @@
-use lb_vrf::lbvrf::LBVRF;
-use lb_vrf::param::Param;
-use lb_vrf::serde::Serdes;
-use lb_vrf::VRF;
-use rand::RngCore;
+use vrf::openssl::{CipherSuite, ECVRF};
+use vrf::VRF;
+
+//use rand::RngCore;
 
 use oqs::*;
 
-fn main() {
-    let mut seed = [0u8; 32];
-    let mut rng = rand::thread_rng();
-    rng.fill_bytes(&mut seed);
+use aes_gcm::{
+    aead::{Aead, KeyInit, OsRng},
+    Aes256Gcm,
+    Nonce, // Or `Aes128Gcm`
+};
 
-    let param: Param = <LBVRF as VRF>::paramgen(seed).unwrap();
-    let (_pk, _sk) = <LBVRF as VRF>::keygen(seed, param).unwrap();
-    let mut buf: Vec<u8> = vec![];
-    let mut buf2: Vec<u8> = vec![];
-    _pk.serialize(&mut buf).unwrap();
-    println!("pk: 0x{:}", hex::encode(&buf));
-    _sk.serialize(&mut buf2).unwrap();
-    println!("sk: 0x{:}", hex::encode(&buf2));
-    let message = "this is a message that vrf signs";
-    let seed = [0u8; 32];
-    let proof = <LBVRF as VRF>::prove(message, param, _pk, _sk, seed).unwrap();
-    let res = <LBVRF as VRF>::verify(message, param, _pk, proof).unwrap();
-    if res.is_some() {
-        println!("Ok!");
-    }
+fn main() {
+    let mut vrf = ECVRF::from_suite(CipherSuite::SECP256K1_SHA256_TAI).unwrap();
+    // Inputs: Secret Key, Public Key (derived) & Message
+    let secret_key =
+        hex::decode("c9afa9d845ba75166b5c215767b1d6934e50c3db36e89b127b8a622b120f6721").unwrap();
+    let public_key = vrf.derive_public_key(&secret_key).unwrap();
+    let message: &[u8] = b"sample";
+
+    // VRF proof and hash output
+    let pi = vrf.prove(&secret_key, message).unwrap();
+    let hash = vrf.proof_to_hash(&pi).unwrap();
+    println!("hash: 0x{:}", hex::encode(&hash));
+
+    // VRF proof verification (returns VRF hash output)
+    let beta = vrf.verify(&public_key, &pi, message).unwrap();
+    println!("beta: 0x{:}", hex::encode(&beta));
 
     let sigalg = sig::Sig::new(sig::Algorithm::Dilithium2).unwrap();
     let kemalg = kem::Kem::new(kem::Algorithm::Kyber512).unwrap();
@@ -54,6 +55,15 @@ fn main() {
         .unwrap();
     let a_kem_ss = kemalg.decapsulate(&kem_sk, &kem_ct).unwrap();
     assert_eq!(a_kem_ss, b_kem_ss);
+
+    let key = Aes256Gcm::generate_key(&mut OsRng);
+    let cipher = Aes256Gcm::new(&key);
+    let nonce = Nonce::from_slice(b"unique nonce"); // 96-bits; unique per message
+    let ciphertext = cipher
+        .encrypt(nonce, b"plaintext message".as_ref())
+        .unwrap();
+    let plaintext = cipher.decrypt(nonce, ciphertext.as_ref()).unwrap();
+    assert_eq!(&plaintext, b"plaintext message");
 
     //Ok(())
 }
