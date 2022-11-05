a1e30735330719c31a34767ab6a430e4a816497d
José Ignacio Escribano <jiep@users.noreply.github.com>
2022-11-05 08:54:45 +0000

    Add basic test for PKE

--- src/pke.rs
+++ src/pke.rs
@@ -46,3 +46,22 @@ pub fn pke_dec(
 
     plaintext
 }
+
+#[cfg(test)]
+mod tests {
+    use oqs::kem;
+    use crate::pke::{pke_enc, pke_dec};
+
+
+    #[test]
+    fn pke_works() {
+        let kemalg = kem::Kem::new(kem::Algorithm::Kyber512).unwrap();
+        let (pk, sk) = kemalg.keypair().unwrap();
+        let m: Vec<u8> = vec![3, 1, 4, 15, 9, 65];
+        let (ct_kem, ct_dem, iv_tag) = pke_enc(&kemalg, &pk, &m);
+
+        let m_decrypted = pke_dec(&kemalg, sk, &ct_kem, &ct_dem, &iv_tag);
+
+        assert_eq!(m, m_decrypted);
+    }
+}
