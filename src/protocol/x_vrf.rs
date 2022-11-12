a3e545793503d620752095d2ee750d2ff1608315
José Ignacio Escribano <jiep@users.noreply.github.com>
2022-11-12 16:44:40 +0000

    Update X-VRF

--- src/protocol/x_vrf.rs
+++ src/protocol/x_vrf.rs
@@ -1,25 +1,69 @@
-use qrllib::rust_wrapper::qrl::{
-    xmss_base::{Sign, XMSSBase, XMSSBaseTrait},
-    xmss_fast::XMSSFast,
+use qrllib::rust_wrapper::xmss_alt::{
+    algsxmss_fast::{xmss_fast_gen_keypair, xmss_fast_sign_msg, BDSState},
+    hash_functions::HashFunction,
+    xmss_common::{xmss_verify_sig, XMSSParams},
 };
 use sha3::{Digest, Sha3_256};
 
-pub fn x_vrf_gen(_seed: &[u8], xmss: &XMSSFast) -> (Vec<u8>, Vec<u8>) {
-    (xmss.get_pk(), xmss.get_sk().to_vec())
+pub fn x_vrf_gen(
+    params: &XMSSParams,
+    state: &mut BDSState,
+    seed: &mut [u8; 48],
+) -> (Vec<u8>, Vec<u8>) {
+    let mut pk: [u8; 64] = [0; 64];
+    let mut sk: [u8; 4 + 4 * 32] = [0; 4 + 4 * 32];
+
+    let _x = xmss_fast_gen_keypair(
+        &HashFunction::Shake128,
+        params,
+        &mut pk,
+        &mut sk,
+        state,
+        seed,
+    )
+    .is_ok();
+
+    (pk.to_vec(), sk.to_vec())
 }
 
-pub fn x_vrf_eval(_ek: &[u8], x: &Vec<u8>, xmss: &mut XMSSFast) -> (Vec<u8>, Vec<u8>) {
-    let pi = xmss.sign(x).unwrap();
+pub fn x_vrf_eval(
+    ek: &mut [u8],
+    x: &Vec<u8>,
+    params: &XMSSParams,
+    state: &mut BDSState,
+) -> (Vec<u8>, Vec<u8>) {
+    let mut pi: [u8; 10000] = [0; 10000];
+
+    let _x = xmss_fast_sign_msg(
+        &HashFunction::Shake128,
+        params,
+        ek,
+        state,
+        &mut pi,
+        x,
+        x.len(),
+    );
+
     let mut hasher = Sha3_256::new();
-    hasher.update([pi.clone(), x.to_vec()].concat());
+    hasher.update([pi.to_vec(), x.to_vec()].concat());
     let y: Vec<u8> = hasher.finalize().to_vec();
 
-    (y, pi)
+    (y, pi.to_vec())
 }
 
-pub fn x_vrf_vfy(vk: &Vec<u8>, mut x: Vec<u8>, y: &[u8], pi: &Vec<u8>) -> bool {
+pub fn x_vrf_vfy(vk: &[u8], x: &mut [u8], y: &[u8], pi: &[u8], params: &XMSSParams) -> bool {
     let mut verification = false;
-    if XMSSBase::verify(&mut x, &pi.to_owned(), vk, None).is_ok() {
+    let h = 4;
+    if xmss_verify_sig(
+        &HashFunction::Shake128,
+        &params.wots_par,
+        x,
+        x.len(),
+        pi,
+        vk,
+        h,
+    ) == 0
+    {
         let mut hasher = Sha3_256::new();
         hasher.update([pi.to_vec(), x.to_vec()].concat());
         let hashed: Vec<u8> = hasher.finalize().to_vec();
--- tests/x_vrf.rs
+++ tests/x_vrf.rs
@@ -1,27 +1,56 @@
 use anon_sym_ake::protocol::x_vrf::{x_vrf_eval, x_vrf_gen, x_vrf_vfy};
-use qrllib::rust_wrapper::qrl::xmss_fast::XMSSFast;
+use qrllib::rust_wrapper::{
+    qrl::xmss_fast::XMSSFast,
+    xmss_alt::{
+        algsxmss_fast::{xmss_fast_gen_keypair, BDSState, TreeHashInst},
+        hash_functions::HashFunction,
+        xmss_common::XMSSParams,
+    },
+};
 
 #[test]
 fn x_vrf_works() {
-    let seed: Vec<u8> = (0..48).collect();
+    let h: u8 = 4;
+    let n: u32 = 48;
+    let mut seed: [u8; 48] = [0; 48];
 
-    let xmss_height: u8 = 8;
+    println!("before keygen");
 
-    let mut xmss = XMSSFast::new(seed.clone(), xmss_height, None, None, None).unwrap();
+    let k: u32 = 2;
+    let stack = vec![0; (h as usize + 1) * n as usize];
+    let stackoffset: u32 = 0;
+    let stacklevels: Vec<u8> = vec![0; h as usize + 1];
+    let auth: Vec<u8> = vec![0; (h as usize) * n as usize];
+    let keep: Vec<u8> = vec![0; (h >> 1) as usize * n as usize];
+    let treehash: Vec<TreeHashInst> = vec![TreeHashInst::default(); h as usize - k as usize];
+    let retain: Vec<u8> = vec![0; ((1 << k) - k - 1) as usize * n as usize];
 
-    let x: Vec<u8> = vec![1, 2, 3, 4];
+    let mut state = BDSState {
+        stack,
+        stackoffset,
+        stacklevels,
+        auth,
+        keep,
+        treehash,
+        retain,
+        next_leaf: 0,
+    };
 
-    let (vk, ek) = x_vrf_gen(&seed, &xmss);
+    let mut params = XMSSParams::new(32, h.into(), 16, 2).unwrap();
 
-    let (y, mut pi) = x_vrf_eval(&ek, &x, &mut xmss);
+    let mut x: Vec<u8> = vec![1, 2, 3, 4];
 
-    let verification = x_vrf_vfy(&vk, x.clone(), &y, &pi);
+    let (vk, mut ek) = x_vrf_gen(&mut params, &mut state, &mut seed);
+
+    let (y, mut pi) = x_vrf_eval(&mut ek, &x, &params, &mut state);
+
+    let verification = x_vrf_vfy(&vk, &mut x.to_vec(), &y, &pi, &params);
 
     assert!(verification);
 
     pi[1] += 2;
 
-    let verification = x_vrf_vfy(&vk, x, &y, &pi);
+    let verification = x_vrf_vfy(&vk, &mut x.to_vec(), &y, &pi, &params);
 
     assert_ne!(verification, true);
 }
