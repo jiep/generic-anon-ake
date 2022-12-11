2260f6342549853f40ce8428d4276c2d834f54c6
José Ignacio Escribano <jiep12345@hotmail.com>
2022-12-11 12:18:01 +0100

    Add PRF

---
+++ tests/prf.rs
@@ -0,0 +1,14 @@
+use anon_sym_ake::protocol::{prf::prf, utils::get_random_key32};
+
+#[test]
+fn prf_works() {
+    let key: Vec<u8> = get_random_key32();
+    let mut counter = 0_u128;
+    let nonce = counter.to_be_bytes();
+    counter += 1;
+    let nonce2 = counter.to_be_bytes();
+    let n = prf(&key, &nonce);
+    let n2 = prf(&key, &nonce2);
+
+    assert_ne!(n, n2);
+}
